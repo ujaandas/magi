ace24dd5c1e0b158e122002fedf7f803b4d127ef
Ujaan Das <ujaandas03@gmail.com>
2025-11-28 16:41:03 +0000

    fresh start

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "a basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    hardware.url = "github:nixos/nixos-hardware/master";
+  };
+
+  outputs = 
+    inputs@{
+      self,
+      nixpkgs,
+      hardware
+    }:
+    {
+      nixosConfigurations = {
+        vm = nixpkgs.lib.nixosSystem {
+	  specialArgs = { inherit self inputs; };
+	  system = "x86_64-linux";
+	  modules = [
+	    ./configuration.nix
+	  ];
+	};
+      };
+    };
+}
